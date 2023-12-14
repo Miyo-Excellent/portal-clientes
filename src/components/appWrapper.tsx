0332a23233c673a8f7137a9d516792c35f18db52
Michell Excellent <theofficesmichell@gmial.com>
2023-12-14 12:03:28 -0400

    Pop Notifications Fix

--- src/components/appWrapper.tsx
+++ src/components/appWrapper.tsx
@@ -1,43 +1,58 @@
 "use client";
 import React, { ReactNode, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ReactNotifications } from "react-notifications-component";
 import { getChatMessagesService } from "@services/chat.service";
 import showNotification from "@helpers/showNotification";
 import Avatar from "@components/avatar";
 import USERS from "@data/userDummyData";
+import { selectChat } from "@redux/slices/chat.slice";
+import { IMessages } from "@data/chatDummyData";
 
 export interface AppWrapperProps {
   children: ReactNode;
 }
 
 export const AppWrapper = ({ children }: AppWrapperProps) => {
+  const { messages } = useSelector(selectChat);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!!messages.length) {
+      const timeout = setTimeout(() => {
+        const message: IMessages | undefined = messages.findLast(
+          ({ user }) => user.id === USERS.JOHN.id,
+        );
+
+        if (message) {
+          showNotification(
+            <span className="d-flex align-items-center">
+              <Avatar
+                src={USERS.JOHN.src.src}
+                size={36}
+                color={USERS.JOHN.color}
+                className="me-3"
+              />
+              <span>{USERS.JOHN.name} sent a message.</span>
+            </span>,
+            <div role="presentation">
+              {message.messages.map(({ id, message }) => (
+                <span key={id}>{message}</span>
+              ))}
+            </div>,
+          );
+        }
+      }, 3000);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [messages]);
 
   useEffect(() => {
     // @ts-ignore
     dispatch(getChatMessagesService());
-
-    const timeout = setTimeout(() => {
-      showNotification(
-        <span className="d-flex align-items-center">
-          <Avatar
-            src={USERS.CHLOE.src.src}
-            size={36}
-            color={USERS.CHLOE.color}
-            className="me-3"
-          />
-          <span>{USERS.CHLOE.name} sent a message.</span>
-        </span>,
-        <div role="presentation">
-          <p>❤️❤️ Hola mi amorcito ❤️❤️</p>
-        </div>,
-      );
-    }, 3000);
-
-    return () => {
-      clearTimeout(timeout);
-    };
   }, []);
 
   return (
